6950feef8dc33f0d32d0d05c477fa84fa19ffc07
MohamedAly8 <mohamed_alyaly@outlook.com>
2020-08-17 05:27:19 -0400

    local changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'dbportfolioM',
+        'PASSWORD': 'django1234',
         'HOST':'localhost',
         'PORT': '5432'
 
